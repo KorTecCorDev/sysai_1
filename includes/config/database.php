03a3312468efb66476d8bfab1b5ea2f55862bb15
Korteccordev <robertokar97@gmail.com>
2025-05-16 17:15:11 -0500

    Commit de prueba

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 function conectarDB() : mysqli {//Indica que retorna una conexión de Mysqli
-    $db= new mysqli('localhost','u612374195_sysai','Tl2&$3Zb','u612374195_sysai');
+    $db= new mysqli('localhost','root','root','u612374195_sysai');
 
     if (!$db) {
         echo 'ERROR no se pudo conectar';
